80c56e68964c748b6430348b3452029eace20d15
Michael Beale <michael.beale@gmail.com>
2013-08-06 16:25:57 -0600

    added account level address and vat_number to address

--- lib/recurly/resource.rb
+++ lib/recurly/resource.rb
@@ -421,7 +421,15 @@ module Recurly
               }
             end
           else
-            record[el.name] = XML.cast el
+            if XML.cast(el).kind_of? Hash
+              recurlyHash = XML.cast el
+              record[el.name] = Address.new
+              recurlyHash.each_pair do |k, v|
+                record[el.name][k] = v
+              end
+            else
+              record[el.name] = XML.cast el
+            end
           end
         end
 
@@ -695,6 +703,7 @@ module Recurly
         node = builder.add_element key
 
         # Duck-typing here is problematic because of ActiveSupport's #to_xml.
+        puts value
         case value
         when Resource, Subscription::AddOns
           value.to_xml options.merge(:builder => node)
